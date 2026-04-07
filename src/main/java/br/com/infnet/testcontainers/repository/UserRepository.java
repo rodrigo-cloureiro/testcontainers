67975c27f6524f16e99f7ef8d56c83ca56696893
rodrigo-cloureiro <rodrigo.cloureiro@al.infnet.edu.br>
2026-04-07 17:16:56 -0300

    adiciona método de consulta por data de nascimento

--- src/main/java/br/com/infnet/testcontainers/repository/UserRepository.java
+++ src/main/java/br/com/infnet/testcontainers/repository/UserRepository.java
@@ -4,10 +4,14 @@ import br.com.infnet.testcontainers.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByName(String name);
+
+    List<User> findByDateOfBirth(LocalDate dateOfBirth);
 }
