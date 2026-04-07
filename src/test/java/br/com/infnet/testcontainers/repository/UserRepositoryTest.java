4f457b573bbe177107e17d9d9f2e8c9e7ae7b5ae
rodrigo-cloureiro <rodrigo.cloureiro@al.infnet.edu.br>
2026-04-07 17:17:43 -0300

    implementa testes de save, findByName e findByDateOfBirth

--- src/test/java/br/com/infnet/testcontainers/repository/UserRepositoryTest.java
+++ src/test/java/br/com/infnet/testcontainers/repository/UserRepositoryTest.java
@@ -2,9 +2,13 @@ package br.com.infnet.testcontainers.repository;
 
 import br.com.infnet.testcontainers.BaseRepositoryTest;
 import br.com.infnet.testcontainers.domain.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,15 +18,40 @@ public class UserRepositoryTest extends BaseRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    private final String name = "Teste";
+    private final LocalDate dateOfBirth = LocalDate.of(2001, 3, 28);
+    private User user;
+
+    @BeforeEach
+    public void setUp() {
+        this.user = new User(name, dateOfBirth);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (this.user != null) this.user = null;
+    }
+
     @Test
-    public void shouldSaveAndFindUser() {
-        User user = new User("Teste");
-
-        User saved = userRepository.save(user);
+    public void shouldSave() {
+        User saved = userRepository.save(this.user);
         assertNotNull(saved.getId());
+    }
 
-        Optional<User> found = userRepository.findByName("Teste");
+    @Test
+    public void shouldFindUserByName() {
+        userRepository.save(this.user);
+
+        Optional<User> found = userRepository.findByName(name);
         assertTrue(found.isPresent());
-        assertEquals("Teste", found.get().getName());
+        assertEquals(name, found.get().getName());
+    }
+
+    @Test
+    public void shouldFindUserByDateOfBirth() {
+        userRepository.save(this.user);
+
+        List<User> found = userRepository.findByDateOfBirth(dateOfBirth);
+        assertFalse(found.isEmpty());
     }
 }
