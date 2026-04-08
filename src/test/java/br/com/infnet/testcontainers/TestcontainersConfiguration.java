9964c6c23848297bae29b7e881e436e0523da308
rodrigo-cloureiro <rodrigo.cloureiro@al.infnet.edu.br>
2026-04-08 02:08:56 -0300

    adiciona configuração do RedisContainer para testes com Testcontainers

--- src/test/java/br/com/infnet/testcontainers/TestcontainersConfiguration.java
+++ src/test/java/br/com/infnet/testcontainers/TestcontainersConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.infnet.testcontainers;
 
+import com.redis.testcontainers.RedisContainer;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -9,15 +10,19 @@ import org.testcontainers.utility.DockerImageName;
 @TestConfiguration(proxyBeanMethods = false) // @Configuration no contexto de testes
 public class TestcontainersConfiguration {
 
-    @SuppressWarnings(value = "resource")
     @Bean // Cria um bean spring
     @ServiceConnection
         // substitui @DynamicPropertySource
         // Configura automaticamente a aplicação (injeta datasource url, username e password)
     PostgreSQLContainer postgresContainer() {
         // sobe um container docker usando a imagem postgres:latest
-        return new PostgreSQLContainer(DockerImageName.parse("postgres:15-alpine"))
-                .withReuse(true);
+        return new PostgreSQLContainer(DockerImageName.parse("postgres:15-alpine"));
+    }
+
+    @Bean
+    @ServiceConnection
+    RedisContainer redisContainer() {
+        return new RedisContainer(DockerImageName.parse("redis:8.6.2-alpine"));
     }
 
 }
