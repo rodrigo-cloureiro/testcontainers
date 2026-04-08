657a8b8ba64f75afc0c024505c1b0f7833914894
rodrigo-cloureiro <rodrigo.cloureiro@al.infnet.edu.br>
2026-04-07 23:56:08 -0300

    habilita cache na aplicação

--- src/main/java/br/com/infnet/testcontainers/TestcontainersApplication.java
+++ src/main/java/br/com/infnet/testcontainers/TestcontainersApplication.java
@@ -2,8 +2,10 @@ package br.com.infnet.testcontainers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TestcontainersApplication {
 
     public static void main(String[] args) {
